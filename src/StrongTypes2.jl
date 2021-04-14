0fc36f99a571594f2db41049ab6833ea4614b553
mobius-eng <mobius-eng.github@protonmail.com>
2021-04-14 15:13:33 +0200

    exporting option for def_strong_type

--- src/StrongTypes2.jl
+++ src/StrongTypes2.jl
@@ -9,11 +9,16 @@ Base.convert(::Type{T}, x::U) where {T, U <: AbstractStrongType{T}} = x.value
 
 (::Type{T})(x::U) where {T, U <: AbstractStrongType{T}} = x.value
 
-macro def_strong_type(name)
+macro def_strong_type(name, exporting = false)
     quote
         struct $(esc(name)){T} <: AbstractStrongType{T}
             value::T
         end
+        $(
+            if exporting
+                :(export $(esc(name)))
+            end
+        )
     end
 end
 
